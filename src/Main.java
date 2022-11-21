09f21ebd51a13ce21af13f874649f6f53ccfcbe1
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-21 12:30:27 +0530

    development

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
+    int choice;
+        Scanner sc=new Scanner(System.in);
 
+        {
+            System.out.println("Press 1 for largest of 3 numbers ");
+            System.out.println("Press 2 for smallest of 3 numbers ");
+            System.out.println("Press 3 for check for prime number ");
+            System.out.println("Press 4 for check even or odd ");
+            System.out.println("Press 5 to reverse ");
+            System.out.println("Press 6 to Quit ");
+
+            System.out.println("Make your choice");
+            choice = sc.nextInt();
+        }
 
     }
 }
